86d258bb023a31ea11d6f7e1b6fbbd1286d735d1
Vladislavfite <117209322+Vladislavfite@users.noreply.github.com>
2025-07-18 14:08:38 +0500

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,2 +1,14 @@
-// Заглушка логики игры
-console.log("Game started");
+// Отрисовка карты при старте игры
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+const mapImage = new Image();
+mapImage.src = 'assets/map/map.png';
+
+mapImage.onload = () => {
+  canvas.width = mapImage.width;
+  canvas.height = mapImage.height;
+  ctx.drawImage(mapImage, 0, 0);
+};
+
+console.log('Game started');
