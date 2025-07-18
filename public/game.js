5a97a7724a6f3f3c931a3ddbba5bcf7910722cb3
Vladislavfite <117209322+Vladislavfite@users.noreply.github.com>
2025-07-18 14:18:14 +0500

    Update game.js

--- public/game.js
+++ public/game.js
@@ -2,13 +2,26 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
 const mapImage = new Image();
 mapImage.src = 'assets/map/map.png';
 
+function drawMap() {
+  ctx.drawImage(mapImage, 0, 0, canvas.width, canvas.height);
+}
+
 mapImage.onload = () => {
-  canvas.width = mapImage.width;
-  canvas.height = mapImage.height;
-  ctx.drawImage(mapImage, 0, 0);
+  resizeCanvas();
+  drawMap();
 };
+
+window.addEventListener('resize', () => {
+  resizeCanvas();
+  drawMap();
+});
 
 console.log('Game started');
